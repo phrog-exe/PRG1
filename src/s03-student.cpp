a3a9166d79b8bdceaef0b40b917c5890eb4353a0
s24404 <s24404@pjwstk.edu.pl>
2021-11-23 19:11:21 +0100

    23.11

--- src/s03-student.cpp
+++ src/s03-student.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <s03-student.h>
+#include "../include/s03-student.h"
 #include <sstream>
 
-s24404::Student::Student(std::string name, std::string surname, std::string index, int semester double gpa)
+s24404::Student::Student(std::string name, std::string surname, std::string nrindex, int semester, float gpa)
 
- : name{name}, surname{surname}, index{index}, semester{semester}, gpa{gpa} {}
+ : name{name}, surname{surname}, nrindex{nrindex}, semester{semester}, gpa{gpa} {}
 
 auto s24404::Student::to_string() const ->std::string {
      auto output =std::ostringstream{};
-     output << "Name: " << name <<"\n" << "Surname: " << surname <<"\n" << "Index number: " << index <<"\n"
+     output << "Name: " << name <<"\n" << "Surname: " << surname <<"\n" << "Index number: " << nrindex <<"\n"
             << "Semester: " << semester <<"\n" << "Grade point average: " << gpa;
 
   return output.str();
@@ -16,7 +16,7 @@ auto s24404::Student::to_string() const ->std::string {
 
 
 auto main() -> int {
-    auto student = s24404::Student{"Weronika", "Głódkowska", "s24404", 1, 3.9};
+    auto student = s24404::Student{"Weronika", "Głódkowska", "s24404", 1, 4.9};
     std::cout <<student.to_string() <<"\n";
 
 return 0;
